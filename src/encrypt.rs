3e8fd189b885af425fdb4305ae5f6e6a1c77f944
Penn Wilbert <pwilbert@gmail.com>
2020-08-18 11:04:44 -0400

    fixed init for encrypt

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -18,15 +18,9 @@ pub struct Encrypt {
 
 impl Encrypt {
 
-    pub fn new(key: Vec<u8>, mode: AesMode, iv: InitializationValue) -> Encrypt {
-        Encrypt {
-            expanded_key: expander::expand(&key),
-            rounds: Self::get_rounds(key.len()),
-            mode,
-            block_size: key.len(),
-            iv,
-        }
-    }
+    // pub fn new(key: Vec<u8>, mode: AesMode, iv: InitializationValue) -> Encrypt {
+        
+    // }
 
     fn get_rounds(key_len: usize) -> u32 {
         match key_len {
@@ -37,19 +31,40 @@ impl Encrypt {
         }
     }
 
-    pub fn start_ecb(self, input: Vec<u8>) -> Vec<u8> {
-        modes::ecb_encrypt::run(self, input)
+    pub fn ecb(key: Vec<u8>, mode: AesMode) -> Encrypt {
+        Encrypt {
+            expanded_key: expander::expand(&key),
+            rounds: Self::get_rounds(key.len()),
+            mode,
+            block_size: key.len(),
+            iv: InitializationValue::None,
+        }
     }
 
-    pub fn start_cbc(mut self, input: Vec<u8>) -> Vec<u8> {
-        let iv = match self.iv {
-            InitializationValue::IV(v) => v,
-            InitializationValue::None => iv_builder::get_iv(self.block_size),
-        };
-        self.iv = InitializationValue::IV(iv.clone());
-        let results = modes::cbc_encrypt::run(&self, input, iv);
+    pub fn cbc(key: Vec<u8>, mode: AesMode, iv: InitializationValue) -> Encrypt {
+        Encrypt {
+            expanded_key: expander::expand(&key),
+            rounds: Self::get_rounds(key.len()),
+            mode,
+            block_size: key.len(),
+            iv,
+        }
 
-        results
+    }
+
+    pub fn run(mut self, input: Vec<u8>) -> Vec<u8> {
+        match self.mode {
+            AesMode::ECB => modes::ecb_encrypt::run(self, input),
+            AesMode::CBC => {
+                let iv: Vec<u8> = match self.iv {
+                    InitializationValue::None => iv_builder::get_iv(self.block_size),
+                    InitializationValue::IV(v) => v,
+                };
+                self.iv = InitializationValue::IV(iv.clone());
+                
+                modes::cbc_encrypt::run(&self, input, iv)
+            },
+        }
     }
 }
 
@@ -67,8 +82,8 @@ use crate::utils::{hex_encoders, printer};
         let input = "Single block msg".as_bytes().to_vec();
         let cipher_answer: Vec<u8> = vec![0xe3, 0x53, 0x77, 0x9c, 0x10, 0x79, 0xae, 0xb8, 0x27, 0x08, 0x94, 0x2d, 0xbe, 0x77, 0x18, 0x1a];
 
-        let encryptor: Encrypt = Encrypt::new(key, AesMode::CBC, iv);
-        let results = encryptor.start_cbc(input);
+        let encryptor: Encrypt = Encrypt::cbc(key, AesMode::CBC, iv);
+        let results = encryptor.run(input);
         assert_eq!(results, cipher_answer);
         printer::print_hex_aligned(&results);
 
@@ -80,9 +95,9 @@ use crate::utils::{hex_encoders, printer};
     pub fn test_ecb_encrypt() {
         let input: Vec<u8> = "This is a test of the ability to encrypt and then decrypt the message".as_bytes().to_vec();
         let key: Vec<u8> = "YELLOW SUBMARINE".as_bytes().to_vec();
-        let encryptor: Encrypt = Encrypt::new(key, AesMode::ECB, InitializationValue::None);
+        let encryptor: Encrypt = Encrypt::ecb(key, AesMode::ECB);
 
-        let results = encryptor.start_ecb(input);
+        let results = encryptor.run(input);
         // dbg!(results);
         // dbg!(iv);
         printer::print_hex_aligned(&results);
@@ -92,9 +107,9 @@ use crate::utils::{hex_encoders, printer};
     pub fn test_cbc_encrypt() {
         let input: Vec<u8> = "This is a test of the ability to encrypt and then decrypt the message".as_bytes().to_vec();
         let key: Vec<u8> = "YELLOW SUBMARINE".as_bytes().to_vec();
-        let encryptor: Encrypt = Encrypt::new(key, AesMode::CBC, InitializationValue::None);
+        let encryptor: Encrypt = Encrypt::cbc(key, AesMode::CBC, InitializationValue::None);
 
-        let results = encryptor.start_cbc(input);
+        let results = encryptor.run(input);
         // dbg!(results);
         // dbg!(iv);
         printer::print_hex_aligned(&results);
